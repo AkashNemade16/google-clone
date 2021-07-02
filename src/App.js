dc3a11e0a7ac8edb06ee1cd84e95efe5ce572b1d
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-07-02 01:42:57 -0700

    initialize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react'
 import './App.css';
+import Home from './pages/home';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>HI this is google clone</h1>
+      <Home/>
+      {/* HOME */}
+      {/* SEARCH */}
     </div>
   );
 }
---
+++ src/pages/home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='home'>
+            <h1>Homepage</h1>
+
+            <div className='home_header'>
+
+            </div>
+
+            <div className='home_body'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
