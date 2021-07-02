b0626d8755f743d47bce53edcb8b4da11ee4fef4
Akash Nemade <37910311+sskkyy962@users.noreply.github.com>
2021-07-02 16:15:08 -0700

    header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './App.css';
 import Home from './pages/home';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>HI this is google clone</h1>
-      <Home/>
+      <Router>
+        <Switch>
+          <Route path='/search'>
+            <h1>HI this is google clone</h1>
+          </Route>
+          <Route path='/'>
+            <Home/>
+          </Route>
+        </Switch>
+      </Router>
+      
       {/* HOME */}
       {/* SEARCH */}
     </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,28 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom';
+import './Home.css'
+import AppsIcon from '@material-ui/icons/Apps';
+import{Avatar}from "@material-ui/core";
 function Home() {
     return (
         <div className='home'>
-            <h1>Homepage</h1>
 
             <div className='home_header'>
+                <div className='home_headerLeft'>
+                    <Link to='/about'>About</Link>
+                    <Link to='/store'>store</Link>
+                </div>
+                <div className='home_headerRight'>
+                    <Link to='/gmail'>Gmail</Link>
+                    <Link to='/images'>Images</Link>
+                    <AppsIcon/>
+                    <Avatar/>
+                </div>
 
             </div>
 
             <div className='home_body'>
-
+                <img src=""/>
             </div>
         </div>
     )
